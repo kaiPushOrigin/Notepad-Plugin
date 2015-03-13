67a74ec9d07e8f55936e7e361b0c145357bfea3f
rikkustrife <danicom23@yahoo.ca>
2015-03-13 00:12:22 -0300

    Delete javacompiler.cpp

--- javacompiler.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <iostream>
-#include <windows.h>
-using namespace std;
-int main()
-{
-    ShellExecuteA(NULL, "open", "C:/WINDOWS/system32/cmd.exe", "/k javac C:/Users/Rikku-strife/scripts/Desktop/HelloWorld.java", NULL, SW_SHOW);
-    ShellExecuteA(NULL, "open", "C:/WINDOWS/system32/cmd.exe", "/K java HelloWorld", "C:/Users/Rikku-strife/scripts/Desktop", SW_SHOW);
-}
